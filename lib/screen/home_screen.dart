70acd5c63f80fb157c3cc5fcd14284cacc6e190c
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-03-10 22:57:03 +0530

    Remove unnecessary blank line in home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
-
 class ThemeNotifier extends ChangeNotifier {
   bool _isDarkMode = false;
 
