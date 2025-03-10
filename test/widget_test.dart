4929c8552d54281ec5776029687e1eccb7a898b3
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-03-10 22:57:37 +0530

    Update import path in widget_test.dart to reflect application rename to gender_genie

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gender_predictor/main.dart';
+import 'package:gender_genie/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
